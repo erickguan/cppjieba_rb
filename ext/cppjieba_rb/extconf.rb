9b9af82ba8138159403a582f0df402736a614b97
Erick Guan <fantasticfears@gmail.com>
2017-07-26 10:34:10 +0200

    built bundle should be in the folder

--- ext/cppjieba_rb/extconf.rb
+++ ext/cppjieba_rb/extconf.rb
@@ -18,7 +18,7 @@ dir_config('cppjieba_rb', HEADER_DIRS, LIB_DIRS)
 
 CONFIG["CXXFLAGS"] += " -std=c++11 -O3"
 $CXXFLAGS = "#{$CXXFLAGS} -std=c++11 -O3"
-create_makefile 'cppjieba_rb'
+create_makefile 'cppjieba_rb/cppjieba_rb'
 # respect header changes
 headers = Dir.glob('*.{hpp,h}').join ' '
 File.open 'Makefile', 'a' do |f|
